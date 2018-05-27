6f05728f5e49206ec5490753eafd3a2c0323241b
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-27 14:13:05 -0400

    Add event listeners for buttons. Add navigation links.

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,18 @@ module.exports = {
     entry: './main.js',
     output: {
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              loader: 'babel-loader',
+              query: {presets: ['es2015']}
+            },
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.scss$/, loaders: ["style-loader", "css-loader", "sass-loader"] },
+            {test: /\.(otf)$/, loader: "file-loader"}
+        ]
     }
 };
