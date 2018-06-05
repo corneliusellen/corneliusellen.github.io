8192b7a31ddc586bdba48ee23ccbc6174c06a0f4
Ellen Cornelius <ecorneli@iwu.edu>
2018-06-04 22:46:24 -0600

    Run build.

--- requests/railsAPI.js
+++ requests/railsAPI.js
@@ -3,7 +3,7 @@ const baseURL = () => {
   if (host === "localhost" || host === "127.0.0.1") {
     return `https://outbreak-ready-be.herokuapp.com`
   } else {
-    return `http://localhost:3000`
+    return `https://outbreak-ready-be.herokuapp.com`
   }
 
 }
